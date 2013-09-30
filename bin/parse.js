c6b1b14d28c728314afedfd0efba773bbe4846b0
James Halliday <mail@substack.net>
2013-09-30 17:19:25 +0100

    include line information in the output

--- bin/parse.js
+++ bin/parse.js
@@ -31,13 +31,30 @@ function end () {
     var pending = 0;
     Object.keys(missed).forEach(function (file) {
         pending ++;
-        sources[file] = missed[file];
+        sources[file] = {};
         
         fs.readFile(file, 'utf8', function (err, src) {
             if (err) return next();
             
-            sources[file].forEach(function (range) {
-                range.push(src.slice(range[0], range[1]));
+            var lines = src.split('\n');
+            function findLine (r) {
+                var c = 0;
+                for (var i = 0; i < lines.length; i++) {
+                    c += lines[i].length;
+                    if (c > r[0]) return i;
+                }
+                return i;
+            }
+            
+            sources[file] = [];
+            missed[file].forEach(function (range) {
+                var lineNum = findLine(range);
+                sources[file].push({
+                    range: range,
+                    lineNum: lineNum,
+                    line: lines[lineNum],
+                    code: src.slice(range[0], range[1])
+                });
             });
             next();
         });
