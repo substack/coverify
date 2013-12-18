6936c164ed77de9ceee098f8cfbf75d775ab8dc9
James Halliday <mail@substack.net>
2013-12-18 15:50:50 -0800

    using testling

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-browserify -t . test/ok.js | node | bin/cmd.js \
+browserify test/ok.js | node | bin/cmd.js \
     || (echo FAIL test/ok.js; exit 1)
-browserify -t . test/fail.js | node | bin/cmd.js \
+browserify test/fail.js | node | bin/cmd.js \
     && (echo FAIL test/fail.js; exit 1)
 echo OK
 exit 0
