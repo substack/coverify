bfb759e616bf9b9d916e35c030a58f2322342e1c
James Halliday <mail@substack.net>
2013-12-18 21:46:56 -0800

    fixed the run script logic

--- test/ok.js
+++ test/ok.js
@@ -12,6 +12,20 @@ test('ok ok', function (t) {
     });
 });
 
+test('ok ok (shim)', function (t) {
+    t.plan(3);
+    var xs = [ 10, 5, 24 ];
+    xs.forEach = undefined;
+    
+    forEach(xs, function (n) {
+        var sum = 0;
+        for (var i = 0; i <= n; i++) {
+            sum += i;
+        }
+        t.equal(sum, (n * (n+1)) / 2);
+    });
+});
+
 function forEach (xs, f) {
     if (xs.forEach) return xs.forEach(f);
     for (var i = 0; i < xs.length; i++) {
--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-(browserify test/ok.js | node | bin/cmd.js \
-    || (echo FAIL test/ok.js; exit 1)) \
-&& (browserify test/fail.js | node | bin/cmd.js \
-    && (echo FAIL test/fail.js; exit 1))
-exit $?
+browserify test/ok.js | node | bin/cmd.js \
+    || (echo FAIL test/ok.js; exit 1) || exit 1
+
+browserify test/fail.js | node | bin/cmd.js \
+    && echo FAIL test/fail.js && exit 1
+
+echo OK
